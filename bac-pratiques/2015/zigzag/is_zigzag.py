7745f2adc3fc3331179a5c6e8d2a3ed83aa7a073
Mourad NOUAILI <mouradnouaili1@gmail.com>
2020-10-09 19:13:50 +0100

    Update is_zigzag.py

--- bac-pratiques/2015/zigzag/is_zigzag.py
+++ bac-pratiques/2015/zigzag/is_zigzag.py
@@ -5,3 +5,12 @@ def is_zigzag(n):
         i += 1
         sign = -sign
     return i == len(n)-1
+
+def is_zigzag_python_like(n):
+    sign = int(n[0])-int(n[1])
+    l = []
+    for i in range(1, len(n)-1):
+        l.append(sign * (int(n[i])-int(n[i+1])) < 0)
+        sign = -sign
+        
+    return all(l)
