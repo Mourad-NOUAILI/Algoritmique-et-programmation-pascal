2ad60f2dd002e806506a12ddb6fce3e842369c15
Mourad NOUAILI <mouradnouaili1@gmail.com>
2021-11-23 07:42:52 +0100

    Update is_zigzag.py

--- bac-pratiques/2015/zigzag/is_zigzag.py
+++ bac-pratiques/2015/zigzag/is_zigzag.py
@@ -1,21 +1,12 @@
 # n is a string of digits
 # Time complexity: O(|n|)
 def is_zigzag(n):
-    sign = int(n[0])-int(n[1])
-    i = 1
-    while i < len(n)-1 and sign * (int(n[i])-int(n[i+1])) < 0:
+    s = str(n)
+    i = 0
+    while True:
+        sign = ord(s[i])-ord(s[i+1])
         i += 1
-        sign = -sign
-    return i == len(n)-1
-
-
-# n is a string of digits
-# Time complexity: O(|n|)
-def is_zigzag_python_like(n):
-    sign = int(n[0])-int(n[1])
-    l = []
-    for i in range(1, len(n)-1):
-        l.append(sign * (int(n[i])-int(n[i+1])) < 0)
-        sign = -sign
-        
-    return all(l)
+        if i == len(s)-1 or sign * (ord(s[i])-ord(s[i+1])) >= 0:
+            break
+    
+    return i == len(s)-1
